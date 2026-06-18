bcb9dc118847a2433df53af3f57b45f727941248
mailang <2435960154@qq.com>
2026-06-18 10:13:32 +0800

    commit

---
+++ src/main/java/org/ml/jenkins_study/JenkinsStudyApplication.java
@@ -0,0 +1,13 @@
+package org.ml.jenkins_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsStudyApplication.class, args);
+    }
+
+}
